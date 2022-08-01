9cc8fd3eb50b61f5520c7848ad073909ca5bf247
Bernario21 <92276432+Bernario21@users.noreply.github.com>
2022-08-02 11:28:31 +1200

    Week 6 (Nuke Timer)
    + A timer for the nuke

--- Assets/Scripts/CSS_NukeTimer.cs
+++ Assets/Scripts/CSS_NukeTimer.cs
@@ -3,38 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CSS_TNukeTrap : CSS_Trap
+public class CSS_NukeTimer : CSS_TNukeTrap
 {
-    public float TimeLeft;
-    public bool TimerOn = false;
+    //public float TimeLeft;
+    //public bool TimerOn = false;
 
-    public Text TimerTxt;
-
-    void Awake()
+    //public Text TimerTxt;
+    // Start is called before the first frame update
+    void Start()
     {
         TimerOn = true;
-        this.SetLifeTime(5.0f);
-        this.SetDamage(-1000);
     }
 
+    // Update is called once per frame
     void Update()
-    {
-        Deterioration();
-        NukeTimer();
-
-    }
-
-    public override void Deterioration()
-    {
-        this.SetLifeTime(this.GetLifeTime() - (this.GetTimeModifier() * Time.deltaTime));
-        if (this.GetLifeTime() <= 0.0f)
-        {
-            CSS_GameManager.Instance.playerRef.transform.GetComponent<CSS_Player>().ModifyHP(this.GetDamage());
-            Object.Destroy(this.gameObject);
-        }
-    }
-
-    public void NukeTimer()
     {
         if (TimerOn)
         {
@@ -51,6 +33,7 @@ public class CSS_TNukeTrap : CSS_Trap
             }
         }
     }
+
     void updateTimer(float currentTime)
     {
         currentTime += 1;
